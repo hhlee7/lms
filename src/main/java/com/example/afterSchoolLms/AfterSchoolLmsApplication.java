f09e859152ccbe2bcbcba338e7e0079c9d2d1e94
okonys <62321546+ysk1007@users.noreply.github.com>
2025-06-10 17:25:35 +0900

    [윤성권] 빈 프로젝트 생성
    .gitignore, 프로젝트 생성

---
+++ src/main/java/com/example/afterSchoolLms/AfterSchoolLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.afterSchoolLms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterSchoolLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterSchoolLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/afterSchoolLms/AfterSchoolLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afterSchoolLms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterSchoolLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
