e4862750cea70b28f0d16233f48c2c9b10ce1435
okonys <62321546+ysk1007@users.noreply.github.com>
2025-06-23 10:20:43 +0900

    [윤성권] AfterSchoolLmsApplication.java 수정
    MapperScan 추가

--- src/main/java/com/example/afterSchoolLms/AfterSchoolLmsApplication.java
+++ src/main/java/com/example/afterSchoolLms/AfterSchoolLmsApplication.java
@@ -1,5 +1,6 @@
 package com.example.afterSchoolLms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
+@MapperScan("com.example.afterSchoolLms.mapper")
 public class AfterSchoolLmsApplication {
 
 	public static void main(String[] args) {
